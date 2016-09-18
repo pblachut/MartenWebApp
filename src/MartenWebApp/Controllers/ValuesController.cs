01819e5f80bef456bed4165ad229ed5b4a2829b6
Piotr Błachut <blachut.piotr@gmail.com>
2016-09-18 17:48:20 +0200

    add marten reference

--- src/MartenWebApp/Controllers/ValuesController.cs
+++ src/MartenWebApp/Controllers/ValuesController.cs
@@ -1,25 +1,57 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using System.Web.Http;
+using Marten;
 
 namespace MartenWebApp.Controllers
 {
     public class ValuesController : ApiController
     {
-        // GET api/values
-        public IEnumerable<string> Get()
+
+        private IDocumentStore GetDocumentStore()
         {
-            return new string[] { "value1", "value2" };
+            return DocumentStore.For(options =>
+            {
+                options.Connection("host=localhost;database=marten_test;password=admin;username=postgres");
+                options.AutoCreateSchemaObjects = AutoCreate.All;
+            });
+        }
+
+        // GET api/values
+        public async Task<IList<User>> Get()
+        {
+            var store = GetDocumentStore();
+
+            using (var session = store.LightweightSession())
+            {
+                return await session.Query<User>().ToListAsync();
+            }
         }
 
         // GET api/values/5
-        public string Get(int id)
+        public async Task<User> Get(Guid id)
         {
-            return "value";
+            var store = GetDocumentStore();
+
+            using (var session = store.LightweightSession())
+            {
+                return await session.Query<User>().SingleAsync(user => user.Id == id);
+            }
         }
 
         // POST api/values
         public void Post([FromBody]string value)
         {
+            var store = GetDocumentStore();
+
+            using (var session = store.LightweightSession())
+            {
+                var user = new User { FirstName = "Han", LastName = "Solo" };
+                session.Store(user);
+
+                session.SaveChanges();
+            }
         }
 
         // PUT api/values/5
---
+++ src/MartenWebApp/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MartenWebApp
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool Internal { get; set; }
+        public string UserName { get; set; }
+
+        public User()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
